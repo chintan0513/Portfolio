1cc39f4f3bec18d885df64440be12fe708694e38
chintan0513 <chintantripathi1310@gmail.com>
2022-11-17 10:42:23 +0530

    theme done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,9 @@ const Footer = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 </p>
             </div>
+            <div className="social-media">
+                
+            </div>
         </div>
 
      );
