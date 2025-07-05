a3bf22f652fe5e585cbe6217f23a9218905ef8c6
chintan0513 <chintantripathi1310@gmail.com>
2025-07-05 13:56:37 -0400

    changes in profile/about component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,26 +1,36 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
-import '../styles/landing.css';
+import "../styles/landing.css";
 
 const Landing = () => {
-    return (
-        <div className='landing' id="home">
-            <div className="intro">
-                <div className="name">
-                    <span>Hi<div className="hand">👋🏻</div></span>
-                    <span className='my-name'>I'm Chintan Tripathi</span>
-                </div>
-                <div className="typewrite">
-                    <Typewriter onInit={(typewriter) => {
-                        typewriter.typeString("CODER").pauseFor(1000).deleteAll()
-                            .typeString("TRAVELER").pauseFor(1000).deleteAll()
-                            .typeString("FOODIE").pauseFor(1000).start();
-                    }} />
-                </div>
-
-            </div>
+  return (
+    <div className="landing" id="home">
+      <div className="intro">
+        <div className="name">
+          <span>
+            Hi<div className="hand">👋🏻</div>
+          </span>
+          <span className="my-name">I'm Chintan Tripathi</span>
         </div>
-    );
-}
+        <div className="typewrite">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Full Stack Developer")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("Data Engineer")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("Cloud Enthusiast")
+                .pauseFor(1000)
+                .start();
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
